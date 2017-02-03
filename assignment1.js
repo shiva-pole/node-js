981ac50e5687c1e91e1922e13be728962075b65e
shiva89 <shiva.pole@talentica.com>
2017-02-03 19:18:05 +0530

    todo list complete

--- assignment1.js
+++ assignment1.js
@@ -16,20 +16,48 @@ Task.prototype.complete = function(){
 }
 
 function TodoList(){
-
     this.tasks = [];
+    this.lastItemId = 0;
 };
 
 //methods
-TodoList.prototype.add = function(){
-
+TodoList.prototype.add = function(taskDesc){
+   var taskId =  this.lastItemId + 1;
+   this.tasks.push(new Task(taskId, taskDesc));
+   this.lastItemId = taskId;
 }
 
-TodoList.prototype.remove = function(){
-
+TodoList.prototype.remove = function(task){
+//    if(Task.prototype.isPrototypeOf(task)){
+      let index = this.tasks.indexOf(task);console.log(index);
+      if(index != -1){
+         this.tasks.splice(index,1);
+      }
+//    }
 }
 
 TodoList.prototype.list = function(){
-    
+    this.tasks.forEach(function(task) {
+        console.log('Task ' + JSON.stringify(task));
+    });
 }
+
+var groceryList = new TodoList();
+
+groceryList.add('bread');
+groceryList.add('cheese');
+groceryList.add('milk');
+
+// console.log(groceryList.tasks);
+
+var breadTask = groceryList.tasks[0];
+
+breadTask.complete();
+
+groceryList.list();
+
+groceryList.remove(breadTask);
+
+groceryList.list();
+
 
